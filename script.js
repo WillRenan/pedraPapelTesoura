5ec3cd3ab48a7faa20bf58cdacf66b76d7bf5a64
WillRenan <willdesign.will@gmail.com>
2021-06-30 20:43:35 -0300

    selected options rock paper scissor

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-var elements =  document.querySelectorAll('.player-options img');
+const playerOne =  document.querySelectorAll('.player-options img');
+const pcPlayer =  document.querySelectorAll('.enemy-options img');
 
-function resetOpacityPlayer(){
-  for (let i = 0; i < elements.length; i++) {
-    elements[i].style.opacity = 0.3;
+var playerOpt=  ' ';
+
+function resetOpacityPlayer(element){ // reseta opacidade das opções n escolhidas
+  for (let i = 0; i < element.length; i++) {
+    element[i].style.opacity = 0.3;
     
   }
 }
 
-for (var i = 0; i < elements.length; i++) {
- 
+function playerPc(){ //faz a jogada do computador
+  let rand = Math.floor(Math.random()*3);
 
-  elements[i].addEventListener('click',(t)=>{
-      resetOpacityPlayer();
-      t.target.style.opacity = 1;    
+  for (let i = 0; i < pcPlayer.length; i++) {
+
+    if (i == rand) {
+      resetOpacityPlayer(pcPlayer);
+      pcPlayer[i].style.opacity = 1;
+     
+    }
+  }
+
+}
+
+for (var i = 0; i < playerOne.length; i++) {
+
+  playerOne[i].addEventListener('click',function(t){
+      resetOpacityPlayer(playerOne);
+
+      t.target.style.opacity = 1;  
+      playerOpt = t.target.getAttribute('opt') ;
+      //alert(playerOpt);
+
+      playerPc();
   });
   
 }
