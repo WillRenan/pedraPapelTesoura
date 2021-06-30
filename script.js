58441b9fa5e7c84b6c5e06086b10e959e5e23422
WillRenan <willdesign.will@gmail.com>
2021-06-30 17:27:32 -0300

    styling page

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert('teste');
+var elements =  document.querySelectorAll('.player-options img');
+
+function resetOpacityPlayer(){
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.opacity = 0.3;
+    
+  }
+}
+
+for (var i = 0; i < elements.length; i++) {
+ 
+
+  elements[i].addEventListener('click',(t)=>{
+      resetOpacityPlayer();
+      t.target.style.opacity = 1;    
+  });
+  
+}
